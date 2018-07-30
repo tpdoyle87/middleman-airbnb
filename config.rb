f62d58e048e796d582a078ae33174b3b9868b0a9
Thomas DoyleEngler <tpdoyle87@gmail.com>
2018-07-30 16:41:33 -0300

    adding the dynamic pages for the middleman project

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+data.flats.each do |name, _info|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
